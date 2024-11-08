f9ae49dfb40ba4d7c4a5c1cb9f546d399af75f51
Colin <cstrassergh@gmail.com>
2024-11-07 19:49:38 -0600

    feat: add header, ItemCard, Main and WeatherCard functionality/styles. add images & fonts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 import './App.css'
+import Header from '../Header/Header';
+import Main from '../Main/Main'
 
 function App() {
+  const [weatherData, setWeatherData] = useState({ type: "cold" });
 
-  return <div>
-    <h1>asdfasdf</h1>
-  </div>;
+  return (
+    <div className='app'>
+      <div className="app__content">
+        <Header />
+        <Main weatherData={weatherData} />
+      </div>
+    </div>
+  );
 }
 
 export default App
